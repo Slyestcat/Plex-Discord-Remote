ca6fe6b015e4d53c8f312e48d3117d46510a0225
Jarred Goddard <jarredgoddard@gmail.com>
2020-04-08 00:36:52 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
-import random
-
+import asyncio
 import plexapi
-from discord.ext import commands
+import discord
 
+from discord.ext import commands
 from plexapi.server import PlexServer
 baseurl = 'http://192.168.1.149:32400'
 token = 'XC9BUftGQAfynjnqSg9u'
@@ -14,67 +14,102 @@ TOKEN = 'Njk2ODI1MTYyMDIwMTU5NTcx.XousXg.fjvr47F4hFYLDvkTf6MXuEr_gVY'
 bot = commands.Bot(command_prefix='$')
 
 plexClient = "OSRSWIN10"
+roleID = 697273095769292900
+
+def pred(m):
+    return m.author == message.author and m.channel == message.channel
 
 @bot.command(name='pause', help='Pauses the currently playing movie')
+@commands.has_any_role(*[roleID]) 
 async def pause(ctx):
     client = plex.client(plexClient)
     client.pause()
     
 @bot.command(name='resume', help='Resumes the currently playing movie')
-async def pause(ctx):
+@commands.has_any_role(*[roleID]) 
+async def resume(ctx):
     client = plex.client(plexClient)
     client.play()
     
 @bot.command(name='stop', help='Stops the currently playing movie')
-async def pause(ctx):
+@commands.has_any_role(*[roleID]) 
+async def stop(ctx):
     client = plex.client(plexClient)
     client.stop()
     
 @bot.command(name='previous', help='Returns to the previous title')
-async def pause(ctx):
+@commands.has_any_role(*[roleID]) 
+async def previous(ctx):
     client = plex.client(plexClient)
     client.skipPrevious()
 
 @bot.command(name='next', help='Skips to the next title')
-async def pause(ctx):
+@commands.has_any_role(*[roleID])
+async def next(ctx):
     client = plex.client(plexClient)
     client.skipNext()
     
 @bot.command(name='fastforward', help='Skips to the next title')
-async def pause(ctx):
+@commands.has_any_role(*[roleID]) 
+async def fastforward(ctx):
     client = plex.client(plexClient)
     client.stepForward()
     
 @bot.command(name='rewind', help='Skips to the next title')
-async def pause(ctx):
+@commands.has_any_role(*[roleID]) 
+async def rewind(ctx):
     client = plex.client(plexClient)
     client.stepBack()
  
 @bot.command(name='play', help='Plays a movie')
-async def search(ctx, pClient, movie_title = None):
-    if movie_title is None:
-        await ctx.send("Please follow the syntax `$search <title>`")
-    else:
-        movie = plex.library.section('Movies').get(movie_title)
-        client = plex.client(pClient)
-        client.playMedia(movie)
-        await ctx.send("**Playing Movie:** *" + movie_title + "*")
-        
-@bot.command(name='search', help='Searches for a movie')
-async def search(ctx, movie_title = None):
+async def play(ctx, movie_title = None):
     if movie_title is None:
         await ctx.send("Please follow the syntax `$search <title>`")
     else:
         searchlist = []
+        searchlistFormatted = []
         movies = plex.library.section('Movies')
         for video in movies.search(movie_title):
             searchlist.append(video.title)
         if not searchlist:
             await ctx.send("**Cannot find a title containing** `" + movie_title + "`")
         else:
-            await ctx.send("**Available Titles**" + '\n' + "```" + '\n'.join(map(str, searchlist)) + "```")
+            for i, item in enumerate(searchlist,1):
+                searchlistFormatted.append(str(i) + '. ' + item)
+            await ctx.send("**Available Titles, select the number of the corresponding title to play**" + '\n' + "```" + '\n'.join(map(str, searchlistFormatted)) + "```")
+            try:
+                msg = await bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
+            except asyncio.TimeoutError:
+                await ctx.send('**You took too long...**')
+            else:
+                movieNum = int(msg.content) - 1
+                client = plex.client(plexClient)
+                cars = plex.library.section('Movies').get(searchlist[movieNum])
+                client.playMedia(cars)
+                await ctx.send("**Playing Movie:** *" + searchlist[movieNum] + "*")
+            
+        
+@bot.command(name='search', help='Searches for a movie')
+@commands.has_any_role(*[roleID])
+async def search(ctx, movie_title = None):
+    if movie_title is None:
+        await ctx.send("Please follow the syntax `$search <title>`")
+    else:
+        searchlist = []
+        searchlistFormatted = []
+        movies = plex.library.section('Movies')
+        for video in movies.search(movie_title):
+            searchlist.append(video.title)
+        if not searchlist:
+            await ctx.send("**Cannot find a title containing** `" + movie_title + "`")
+        else:
+            for i, item in enumerate(searchlist,1):
+                searchlistFormatted.append(str(i) + '. ' + item)
+            await ctx.send("**Available Titles**" + '\n' + "```" + '\n'.join(map(str, searchlistFormatted)) + "```")
+
 
 @bot.command(name='clients', help='Lists all connected clients')
+@commands.has_any_role(*[roleID])
 async def search(ctx):
     searchlist = []
     for client in plex.clients():
