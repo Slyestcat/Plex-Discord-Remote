2062ab524c060c8bb6f702013d2238b422c81f40
Jarred Goddard <jarredgoddard@gmail.com>
2020-04-18 20:16:53 -0500

    updated to include imdb metadata
    added metadata to the commands and added $info command

--- bot.py
+++ bot.py
@@ -3,15 +3,18 @@ import asyncio
 import plexapi
 import discord
 
+from imdb import IMDb
 from discord.ext import commands
 from plexapi.server import PlexServer
-baseurl = 'http://192.168.1.149:32400'
-token = 'XC9BUftGQAfynjnqSg9u' #TODO: set plex data in another file
+baseurl = 'http://192.168.1.13:32400'
+token = 'CTVRSarGZJ2y7Tj_ngez' #TODO: set plex data in another file
 plex = PlexServer(baseurl, token)
 
 TOKEN = 'Njk2ODI1MTYyMDIwMTU5NTcx.XousXg.fjvr47F4hFYLDvkTf6MXuEr_gVY' #TODO: set bot token in another file
    
-bot = commands.Bot(command_prefix='$') #TODO: switch all "$" flags to be dynamic in bot responses
+bot = commands.Bot(command_prefix='$', case_insensitive=True) #TODO: switch all "$" flags to be dynamic in bot responses
+
+ia = IMDb()
 
 plexClient = "OSRSWIN10" #TODO: set client in another file
 roleID = 697273095769292900 #TODO: set roleID in another file
@@ -22,8 +25,6 @@ def pred(m):
 def get_ms(time_str):
     h, m, s = time_str.split(':')
     return int(h) * 3600000 + int(m) * 60000 + int(s) * 1000
-
-
 
 @bot.command(name='pause', help='Pauses the currently playing movie')
 @commands.has_any_role(*[roleID]) 
@@ -58,7 +59,7 @@ async def previous(ctx):
 async def next(ctx):
     client = plex.client(plexClient)
     client.skipNext()
-    await ctx.send("Skipped to the next movie in the queue.")
+    await ctx.send("Skipped to the next movie in the queue")
     
 @bot.command(name='fforward', help='Skips forward a chunk')
 @commands.has_any_role(*[roleID]) 
@@ -109,7 +110,7 @@ async def rewind(ctx,x):
     
 @bot.command(name='getsubtitles', help='Gets the availble subtitles for the specified movie') #TODO: allow used to select subtitle stream from this command
 @commands.has_any_role(*[roleID]) 
-async def rewind(ctx,x = None):
+async def rewind(ctx, *, x = None):
     embed=discord.Embed(title="Available Subtitles", color=0x00ff08)
     embed.set_footer(text="Please use the $subtitles command to enable a subtitles stream.")
     embed2=discord.Embed(title="Available Titles", color=0x00ff08)
@@ -127,7 +128,7 @@ async def rewind(ctx,x = None):
             await ctx.send("Cannot find a title containing `" + x + "`")
         else:
             for i, item in enumerate(searchlist):
-                searchlistFormatted.append(str(i+1) + '. ' + item)
+                searchlistFormatted.append(str(i+1) + '. ' + item  + " (" + str(video.year) + ")")
             embed2.add_field(name="Movies", value="```" + '\n'.join(map(str, searchlistFormatted)) + "```", inline=True)
             await ctx.send(embed=embed2)
             try:
@@ -135,7 +136,7 @@ async def rewind(ctx,x = None):
             except asyncio.TimeoutError:
                 await ctx.send('You took too long...')
             else:
-                movieNum = int(msg.content)
+                movieNum = int(msg.content) - 1
                 movies = plex.library.section("Movies")
                 for movie in movies.get(searchlist[movieNum]):
                     movie = movie.reload()
@@ -155,7 +156,7 @@ async def rewind(ctx):
         print(''.join([x.file for x in media_list.iterParts()]))
 
 @bot.command(name='play', help='Plays a movie') #TODO: add catch if movie is paused and output text to tell user to use $resume to resume the movie
-async def play(ctx, movie_title = None):
+async def play(ctx, *, movie_title = None):
     embed=discord.Embed(title="Available Titles", description="Select the number of the corresponding title to play.", color=0x00ff08)
     embed.set_footer(text="Please enter the number of the title you would like to play.")
     if movie_title is None:
@@ -170,7 +171,7 @@ async def play(ctx, movie_title = None):
             await ctx.send("Cannot find a title containing `" + movie_title + "`")
         else:
             for i, item in enumerate(searchlist):
-                searchlistFormatted.append(str(i+1) + '. ' + item)
+                searchlistFormatted.append(str(i+1) + '. ' + item  + " (" + str(video.year) + ")")
             embed.add_field(name="Movies", value="```" + '\n'.join(map(str, searchlistFormatted)) + "```", inline=True)
             await ctx.send(embed=embed)
             try:
@@ -178,16 +179,51 @@ async def play(ctx, movie_title = None):
             except asyncio.TimeoutError:
                 await ctx.send('You took too long...')
             else:
-                movieNum = int(msg.content)
+                movieNum = int(msg.content) - 1
                 client = plex.client(plexClient)
                 cars = plex.library.section('Movies').get(searchlist[movieNum])
                 client.playMedia(cars)
-                embed2=discord.Embed(title=searchlist[movieNum], description="is now playing", color=0xffdd00)
+                movies = ia.search_movie(searchlist[movieNum])
+                id = movies[0].getID()
+                movie = ia.get_movie(id)
+                title = movie['title']
+                try:
+                    year = movie['year']
+                except KeyError:
+                    year = "?"
+                try:
+                    rating = movie['rating']
+                except KeyError:
+                    rating = "?"
+                try:
+                    directors = movie['directors']
+                except KeyError:
+                    directors = "?"
+                try:
+                    casting = movie['cast']
+                except KeyError:
+                    casting = "?"
+                try:
+                    coverURL = movie['cover url']
+                except KeyError:
+                    coverURL = "?"
+                try:
+                    plot = movie['plot']
+                except KeyError:
+                    plot = "?"
+                text = "".join(map(str, plot[0]))
+                head, sep, tail = text.partition("::")
+                embed2=discord.Embed(title=title + "  (" + str(year) + ")", description="Rated " + str(rating) + " out of 10", color=0xffdd00)
+                embed2.set_author(name="Now Playing:")
+                embed2.set_thumbnail(url=coverURL)
+                embed2.add_field(name="Summary", value="```" + head + "```", inline=False)
+                embed2.add_field(name="Cast", value="```" + '\n'.join(map(str, casting[0:5])) + "```", inline=False)
+                embed2.add_field(name="Directors", value="```" + '\n'.join(map(str, directors[0:5])) + "```", inline=False)
                 await ctx.send(embed=embed2)
 
 @bot.command(name='search', help='Searches for a movie') #TODO: add request command that ties into radarr/sonarr
 @commands.has_any_role(*[roleID])
-async def search(ctx, movie_title = None):
+async def search(ctx, *, movie_title = None):
     embed=discord.Embed(title="Available Titles", description="Currently available titles.", color=0x00ff08)
     if movie_title is None:
         await ctx.send("Please follow the syntax `$search <title>`")
@@ -201,7 +237,7 @@ async def search(ctx, movie_title = None):
             await ctx.send("Cannot find a title containing `" + movie_title + "`")
         else:
             for i, item in enumerate(searchlist):
-                searchlistFormatted.append(str(i+1) + '. ' + item)
+                searchlistFormatted.append(str(i+1) + '. ' + item  + " (" + str(video.year) + ")")
         embed.add_field(name="Movies", value="```" + '\n'.join(map(str, searchlistFormatted[0:10])) + "```", inline=True)
         await ctx.send(embed=embed)
 
@@ -216,7 +252,7 @@ async def search(ctx):
         searchlist.append(video.title)
     else:
         for i, item in enumerate(searchlist):
-            searchlistFormatted.append(str(i+1) + '. ' + item)
+            searchlistFormatted.append(str(i+1) + '. ' + item + " (" + str(video.year) + ")")
         embed.add_field(name="Movies", value="```" + '\n'.join(map(str, searchlistFormatted[0:10])) + "```", inline=True)
         await ctx.send(embed=embed)
 
@@ -225,8 +261,78 @@ async def search(ctx):
 async def rewind(ctx):
     client = plex.client(plexClient)
     client.reload()
-    
-    
+
+@bot.command(name='info', help='Gets information about the specified movie')
+@commands.has_any_role(*[roleID]) 
+async def pause(ctx, *, movie_title = None):
+    embed=discord.Embed(title="Available Titles", description="Select the number of the corresponding title to get information about.", color=0x00ff08)
+    embed.set_footer(text="If you cannot find the title, please be more specific in the search")
+    if movie_title is None:
+        await ctx.send("Please follow the syntax `$info <title>`")    
+    else:
+        movielist = []
+        movielistFormatted = []
+        movies = ia.search_movie(movie_title)
+        print("Searching for " + movie_title + ":")
+        for movie in movies:
+            title = movie['title']
+            try:
+                year = movie['year']
+            except KeyError:
+                year = "?"
+            movielist.append(title + " (" + str(year) + ")" )
+        if not movielist:
+            await ctx.send("Cannot find a title containing `" + movie_title + "`")
+        else:
+            for i, item in enumerate(movielist):
+                movielistFormatted.append(str(i+1) + '. ' + item)
+        embed.add_field(name="Movies", value="```" + '\n'.join(map(str, movielistFormatted[0:10])) + "```", inline=True)
+        await ctx.send(embed=embed)
+        print(ia.get_movie_infoset())
+        try:
+            msg = await bot.wait_for('message', check=lambda message: message.author == ctx.author, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.send('You took too long...')
+        else:
+            movieNum = int(msg.content) - 1
+            id = movies[movieNum].getID()
+            movie = ia.get_movie(id)
+            title = movie['title']
+            try:
+                year = movie['year']
+            except KeyError:
+                year = "?"
+            try:
+                rating = movie['rating']
+            except KeyError:
+                rating = "?"
+            try:
+                directors = movie['directors']
+            except KeyError:
+                directors = "?"
+            try:
+                casting = movie['cast']
+            except KeyError:
+                casting = "?"
+            try:
+                coverURL = movie['cover url']
+            except KeyError:
+                coverURL = "?"
+            try:
+                plot = movie['plot']
+            except KeyError:
+                plot = "?"
+            text = "".join(map(str, plot[0]))
+            head, sep, tail = text.partition("::")
+            embed2=discord.Embed(title=title + "  (" + str(year) + ")", description="Rated " + str(rating) + " out of 10", color=0x00ff08)
+            embed2.set_thumbnail(url=coverURL)
+            embed2.add_field(name="Summary", value="```" + head + "```", inline=False)
+            embed2.add_field(name="Cast", value="```" + '\n'.join(map(str, casting[0:5])) + "```", inline=False)
+            embed2.add_field(name="Directors", value="```" + '\n'.join(map(str, directors[0:5])) + "```", inline=False)
+            await ctx.send(embed=embed2)
+            
+           
+
 # @bot.command(name='clients', help='Lists all connected clients') #debugging command
 # @commands.has_any_role(*[roleID])
 # async def search(ctx):
